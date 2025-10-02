8ab4aeaeff5ce577678228713e460972a8d9c445
OzuSus <21130327@st.hcmuaf.edu.vn>
2025-10-02 21:45:15 +0700

    update pricing for hero page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from "@/modules/hero";
 import Features from "../modules/features";
+import Pricing from "@/modules/pricing";
 
 const Page = () => {
     return (
         <div>
             <Hero/>
             <Features/>
+            <Pricing/>
         </div>
     );
 };
